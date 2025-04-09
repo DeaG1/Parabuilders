0a74e4df1562bf7948a94506d157deb3a9aafbb4
DeaG <romulodoprado@gmail.com>
2025-04-08 21:26:31 -0300

    configuration base

---
+++ src/sections/Navbar.tsx
@@ -0,0 +1,17 @@
+// app/components/Navbar.tsx
+import Image from 'next/image'
+import logo from '@/assets/images/logoWithName.png'
+import Link from 'next/link'
+
+const navLinks = [
+  { label: "Home", href: "#" },
+  { label: "Results", href: "#" },
+  { label: "Services", href: "#" },
+  { label: "Social Media", href: "#" },
+];
+
+export default function Navbar() {
+  return (
+    <div></div>
+  );
+}
